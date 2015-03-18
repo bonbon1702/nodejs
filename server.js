2455b93795ed3e605fa326e97c13b4e5b9bc96e0
bonbon1702 <tuannghia1702@gmail.com>
2015-03-18 20:29:00 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+/**
+ * Created by nghia on 18/03/2015.
+ */
+var express =   require('express'),
+    http =      require('http'),
+    server =    http.createServer(app);
+
+var app = express();
+
+const redis =   require('redis');
+const io =      require('socket.io');
+const client =  redis.createClient();
+
+server.listen(3000, 'localhost');
+
+io.listen(server).on('connection', function(client) {
+    const redisClient = redis.createClient()
+    redisClient.subscribe('realTime.notification');
+
+    redisClient.on("message", function(channel, message) {
+        client.emit(channel, message);
+    });
+
+    client.on('disconnect', function() {
+        redisClient.quit();
+    });
+});
